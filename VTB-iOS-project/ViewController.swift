3b0c73cfcabee768af688ce6e1b18ab2f6ded91a
EkaterinaVishnevskaya <ekaterina2-97@yandex.ru>
2020-06-29 14:05:17 +0300

    Init

---
+++ VTB-iOS-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VTB-iOS-project
+//
+//  Created by Екатерина Вишневская - ВТБ on 29.06.2020.
+//  Copyright © 2020 Екатерина Вишневская - ВТБ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
